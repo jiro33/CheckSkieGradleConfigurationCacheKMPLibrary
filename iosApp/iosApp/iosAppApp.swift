e324c90eca76d980f3251826a79877eca7d162e3
jiro33 <nagasaki@sansan.com>
2024-04-17 22:12:25 +0900

    create iosApp

---
+++ iosApp/iosApp/iosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosAppApp.swift
+//  iosApp
+//
+//  Created by Jiro Nagasaki on 2024/04/17.
+//
+
+import SwiftUI
+
+@main
+struct iosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
